16bb39b616a5aaa47370e40f5451742f87b45cbf
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2019-04-05 00:12:39 +0600

    Added index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import Button from './components/Button';
+import LazyImage from './components/LazyImage';
+import Overlay from './components/Overlay';
+import Modal from './components/Modal';
+import Portal from './components/Portal';
+
+export {
+  Button,
+  LazyImage,
+  Modal,
+  Overlay,
+  Portal,
+};
